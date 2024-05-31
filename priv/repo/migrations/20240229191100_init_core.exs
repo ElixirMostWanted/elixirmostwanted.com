19de9be821bd8b618f3b454ee62670d75c2f5945
Beltrán Aceves <beltranaceves@gmail.com>
2024-05-31 04:12:37 +0200

    Delete field in migration for user table

--- priv/repo/migrations/20240229191100_init_core.exs
+++ priv/repo/migrations/20240229191100_init_core.exs
@@ -8,7 +8,6 @@ defmodule ElixirMostWanted.Repo.Migrations.InitCore do
       add :name, :string
       add :token, :string
       add :avatar_url, :string
-      add :external_homepage_url, :string
       add :visibility, :integer, null: false, default: 1
 
       timestamps()
