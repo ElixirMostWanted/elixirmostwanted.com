8534cfb2f00ed4ce20a0c6dd2d799efa522c54a1
Sylvester Roos <sylvester@thejoyofcoding.nl>
2024-07-01 12:38:27 +0200

    add multiple wanteds and votes to test

--- test/elixir_most_wanted/wanteds/wanted.ex
+++ test/elixir_most_wanted/wanteds/wanted.ex
@@ -18,13 +18,27 @@ defmodule ElixirMostWanted.Wanteds.WantedTest do
           returning: true
         )
 
+      wanted2 =
+        Repo.insert!(
+          %Wanted{
+            slug_id: "ecto",
+            user_id: user.id
+          },
+          returning: true
+        )
+
       assert wanted.vote_count == 0
+      assert wanted2.vote_count == 0
 
       Repo.insert!(%Vote{wanted_id: wanted.id})
+      Repo.insert!(%Vote{wanted_id: wanted.id})
+      Repo.insert!(%Vote{wanted_id: wanted2.id})
 
       wanted = Repo.get!(Wanted, wanted.id)
+      wanted2 = Repo.get!(Wanted, wanted2.id)
 
-      assert wanted.vote_count == 1
+      assert wanted.vote_count == 2
+      assert wanted2.vote_count == 1
     end
   end
 end
