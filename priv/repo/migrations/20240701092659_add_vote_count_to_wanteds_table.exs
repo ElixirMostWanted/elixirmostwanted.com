103d4f4aec21b5ec10cb66a1017ee530dea49b2b
Sylvester Roos <sylvester@thejoyofcoding.nl>
2024-07-06 14:55:36 +0200

    add trigger for vote delete

--- priv/repo/migrations/20240701092659_add_vote_count_to_wanteds_table.exs
+++ priv/repo/migrations/20240701092659_add_vote_count_to_wanteds_table.exs
@@ -7,7 +7,7 @@ defmodule ElixirMostWanted.Repo.Migrations.AddVoteCountToWantedsTable do
     end
 
     execute """
-    CREATE OR REPLACE FUNCTION update_wanted_vote_count()
+    CREATE OR REPLACE FUNCTION update_wanted_vote_count_on_insert()
     RETURNS TRIGGER AS $$
     BEGIN
         UPDATE wanteds
@@ -20,10 +20,30 @@ defmodule ElixirMostWanted.Repo.Migrations.AddVoteCountToWantedsTable do
     """
 
     execute """
-    CREATE TRIGGER after_vote_insert
+    CREATE OR REPLACE FUNCTION update_wanted_vote_count_on_delete()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        UPDATE wanteds
+        SET vote_count = vote_count - 1
+        WHERE id = OLD.wanted_id;
+
+        RETURN OLD;
+    END;
+    $$ LANGUAGE plpgsql;
+    """
+
+    execute """
+    CREATE TRIGGER after_vote_insert_update_wanted_vote_count
       AFTER INSERT ON votes
       FOR EACH ROW
-      EXECUTE FUNCTION update_wanted_vote_count();
+      EXECUTE FUNCTION update_wanted_vote_count_on_insert();
+    """
+
+    execute """
+    CREATE TRIGGER after_vote_delete_update_wanted_vote_count
+      AFTER DELETE ON votes
+      FOR EACH ROW
+      EXECUTE FUNCTION update_wanted_vote_count_on_delete();
     """
   end
 end
--- test/elixir_most_wanted/wanteds/wanted.ex
+++ test/elixir_most_wanted/wanteds/wanted.ex
@@ -8,6 +8,7 @@ defmodule ElixirMostWanted.Wanteds.WantedTest do
   describe "wanted" do
     test "update vote_count after related vote insert" do
       user = Repo.insert!(%User{})
+      user2 = Repo.insert!(%User{})
 
       wanted =
         Repo.insert!(
@@ -30,15 +31,58 @@ defmodule ElixirMostWanted.Wanteds.WantedTest do
       assert wanted.vote_count == 0
       assert wanted2.vote_count == 0
 
-      Repo.insert!(%Vote{wanted_id: wanted.id})
-      Repo.insert!(%Vote{wanted_id: wanted.id})
-      Repo.insert!(%Vote{wanted_id: wanted2.id})
+      Repo.insert!(%Vote{wanted_id: wanted.id, user_id: user.id})
+      Repo.insert!(%Vote{wanted_id: wanted.id, user_id: user2.id})
+      Repo.insert!(%Vote{wanted_id: wanted2.id, user_id: user.id})
 
       wanted = Repo.get!(Wanted, wanted.id)
       wanted2 = Repo.get!(Wanted, wanted2.id)
 
       assert wanted.vote_count == 2
       assert wanted2.vote_count == 1
+    end
+
+    test "update vote_count after related vote delete" do
+      user = Repo.insert!(%User{})
+      user2 = Repo.insert!(%User{})
+
+      wanted =
+        Repo.insert!(
+          %Wanted{
+            slug_id: "phoenix",
+            user_id: user.id
+          },
+          returning: true
+        )
+
+      wanted2 =
+        Repo.insert!(
+          %Wanted{
+            slug_id: "ecto",
+            user_id: user.id
+          },
+          returning: true
+        )
+
+      vote1 = Repo.insert!(%Vote{wanted_id: wanted.id, user_id: user.id})
+      _vote2 = Repo.insert!(%Vote{wanted_id: wanted.id, user_id: user2.id})
+      vote3 = Repo.insert!(%Vote{wanted_id: wanted2.id, user_id: user.id})
+
+      wanted = Repo.get!(Wanted, wanted.id)
+      wanted2 = Repo.get!(Wanted, wanted2.id)
+
+      from(v in Vote,
+        where:
+          (v.wanted_id == ^vote1.wanted_id and v.user_id == ^vote1.user_id) or
+            (v.wanted_id == ^vote3.wanted_id and v.user_id == ^vote3.user_id)
+      )
+      |> Repo.delete_all()
+
+      wanted = Repo.get!(Wanted, wanted.id)
+      wanted2 = Repo.get!(Wanted, wanted2.id)
+
+      assert wanted.vote_count == 1
+      assert wanted2.vote_count == 0
     end
   end
 end
